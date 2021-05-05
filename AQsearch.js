97be6d05a30a35c23363fc4248923752947c8b81
sahilpabale <dev.sahilpabale@gmail.com>
2021-05-05 11:08:48 +0530

    done

--- AQsearch.js
+++ AQsearch.js
@@ -2,72 +2,78 @@ globalThis.api_key_two = "db9c36870967334b792cbdecc9527b5b4c834a90";
 export async function getSearchData() {
     const element = document.querySelector("#getInput").value;
     console.log(element);
-    const url = await fetch("https://api.waqi.info/search/?token=" + api_key_two + "&keyword=" + element);
+    const url = await fetch(
+        "https://api.waqi.info/search/?token=" +
+            api_key_two +
+            "&keyword=" +
+            element
+    );
     const search = await url.json();
     const aqis = search.data;
 
-
-    const getCategorisedData = aqi => {
-        let className = 'unknown';
-        let impact = 'unknown';
+    const getCategorisedData = (aqi) => {
+        let className = "unknown";
+        let impact = "unknown";
 
         if (aqi >= 0 && aqi <= 50) {
-            impact = 'Good';
-            className = 'good';
+            impact = "Good";
+            className = "good";
         } else if (aqi >= 51 && aqi <= 100) {
-            impact = 'Moderate';
-            className = 'moderate';
+            impact = "Moderate";
+            className = "moderate";
         } else if (aqi >= 101 && aqi <= 150) {
-            impact = 'Unhealthy for Sensitive Groups';
-            className = 'unhealthy-sentitive';
+            impact = "Unhealthy for Sensitive Groups";
+            className = "unhealthy-sentitive";
         } else if (aqi >= 151 && aqi <= 200) {
-            impact = 'Unhealthy';
-            className = 'unhealthy';
+            impact = "Unhealthy";
+            className = "unhealthy";
         } else if (aqi >= 201 && aqi <= 300) {
-            impact = 'Very Unhealthy';
-            className = 'very-unhealthy';
+            impact = "Very Unhealthy";
+            className = "very-unhealthy";
         } else if (aqi >= 301) {
-            impact = 'Hazardous';
-            className = 'hazardous';
-        } else if (aqi == '') {
-            impact = 'Unknown(No Results Found)';
-            className = 'Unknown';
+            impact = "Hazardous";
+            className = "hazardous";
+        } else if (aqi == "") {
+            impact = "Unknown(No Results Found)";
+            className = "Unknown";
         }
 
-
         let catagorized = {};
-        catagorized['impact'] = impact;
-        catagorized['className'] = className;
+        catagorized["impact"] = impact;
+        catagorized["className"] = className;
 
         return catagorized;
     };
     let html = "";
     if (aqis.length != 0) {
-        aqis.map(element => {
+        aqis.map((element) => {
             const aqi = element.aqi;
-            const sTime = new Date((element.time.stime).replace(/-/g, '/'));
-            const sTimeup = sTime.toLocaleTimeString('en-US');
+            const sTime = new Date(element.time.stime.replace(/-/g, "/"));
+            const sTimeup = sTime.toLocaleTimeString("en-US");
             html += `<div class="${getCategorisedData(aqi).className}">
                                 <div class="header">
                                     <h1 class="aqi_index"> ${aqi} </h1>
-                                    <p class="aqiImpact">${getCategorisedData(aqi).impact}</p>
+                                    <p class="aqiImpact">${
+                                        getCategorisedData(aqi).impact
+                                    }</p>
                                 </div>
                                 <div class="cont">
-                                    <p class="city">  ${element.station.name} </p>
+                                    <p class="city">  ${
+                                        element.station.name
+                                    } </p>
                                     <p> Updated On : ${sTimeup} </p>
                                 </div>
-                            </div>` })
-    }
-    else {
+                            </div>`;
+        });
+    } else {
         html = ` <div class="header">
                         <h1> No Result Found</h1>
-                    </div>`
+                    </div>`;
     }
 
     const def = document.querySelector("#content");
     def.innerHTML = html;
-
-};
+}
 
 const showData = document.querySelector("#btn2");
-showData.addEventListener('click', getSearchData, false);
+showData.addEventListener("click", getSearchData, false);
